ae2010e867289d36fdb7ddbc297adf6d6bb2a25f
Aman Maldar <amanmaldar@users.noreply.github.com>
2017-04-18 16:34:21 -0700

    Create OS_program1B.c

---
+++ OS_program1B.c
@@ -0,0 +1,37 @@
+/*
+Author - 		Aman Maldar 
+Course - 		EECE 5730 - Operating Systems
+Instructor - 	Dr. M. Geiger
+
+Project -		Program 1, Question B (+5 Points)
+
+Problem Statement -		
+Your program creates multiple child processes without using a loop, printing messages at the start and end of each process as 
+described in part A. 
+*/
+
+#include<sys/types.h>
+#include<stdio.h>
+#include<unistd.h>
+
+int main()
+{
+	pid_t pid;
+	//fork a child process
+	pid = fork();
+	
+	if (pid < 0) {
+		fprintf(stderr, "Fork Failed \n");
+		return 1;
+	}
+	else if (pid == 0){//child process 
+		printf("Inside of child. Do something \n");
+		printf("Child process terminates \n");
+		execlp("bin/ls","ls",NULL);
+	}
+	else{//parent process
+	wait(NULL);
+	printf("Child Completes and returns it pid to parent \n");
+	}
+	return 0;
+}
