e780c2dbd16e16794ec64c7156e8c2ca0d511275
Aman Maldar <amanmaldar@users.noreply.github.com>
2017-04-18 15:49:34 -0700

    Update OS_program1.c

--- OS_program1.c
+++ OS_program1.c
@@ -1,1 +1,27 @@
 //the program for assignement 1 goes here
+
+#include<sys/types.h>
+#include<stdio.h>
+#include<unistd.h>
+
+int main
+{
+	pid_t pid;
+	//fork a child process
+	pid = fork();
+	
+	if (pid < 0) {
+		fprintf(stderr, "Fork Failed \n");
+		return 1;
+	}
+	else if (pid == 0){//child process 
+		printf("Inside of child. Do something \n")
+		printf("Child process terminates \n");
+		execlp("bin/ls","ls",NULL);
+	}
+	else{//parent process
+	wait(NULL);
+	printf("Child Completes and returns it pid to parent \n");
+	}
+	retun 0;
+}
