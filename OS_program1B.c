5da3a1237861e529aa90e973be9224c8b46e1093
Aman Maldar <amanmaldar@users.noreply.github.com>
2017-04-18 17:21:10 -0700

    Update OS_program1B.c

--- OS_program1B.c
+++ OS_program1B.c
@@ -17,23 +17,26 @@ described in part A.
 int main()
 {
 	printf("********Project 1 - Question B******** \n");
-	pid_t pid;
+	pid_t pid_1,pid_2,pid_3;
 	//fork a child process
-	pid = fork();
+	pid_1 = fork();
+	pid_2 = fork();
+	pid_3 = fork();
 	
 	if (pid < 0) {
 		fprintf(stderr, "Fork Failed \n");
 		return 1;
 	}
 	else if (pid == 0){		//child process 
-		printf("Child Message  - Inside of child. Do something Interesting. \n");
-		printf("Child Message  - Child process terminates. Fork() returns pid of child = %d \n", pid);
+		printf("Child Message  - Entered Child Process. Do something Interesting. \n");
+		printf("Child Message  - Child Terminates. Fork() returns pid of child = %d to Parent \n", pid);
 		execlp("bin/ls","ls",NULL);
 	}
 	else{		//parent process
-	printf("Parent Message - Parent waiting for child to terminate \n");
+	printf("Parent Message - Entered Parent Process, Parent waiting for child to terminate \n");
 	wait(NULL);
-	printf("Parent Message - Child Completes and returns its pid = %d \n", pid);
+	printf("Parent Message - Child Completes and returns its pid = %d \n", getpid());
+	printf("Parent Message - Parent Exits \n");
 	}
 	
 	return 0;
