21c7bfaabfa9286e815421daee4d686fe6e766f3
Aman Maldar <amanmaldar@users.noreply.github.com>
2017-04-26 09:38:07 -0700

    Update OS_program1.c

--- OS_program1.c
+++ OS_program1.c
@@ -1,10 +1,24 @@
 /*
-Author - 	
+Authors - 	
 1) Aman Maldar (01670909)
 2) Vishnu Vardhan Reddy Donthi (01646354)
+Authors have worked in colaboration on different parts of the project to come up with final solution.
+
 Course - 	EECE 5730 - Operating Systems
 Instructor - 	Dr. M. Geiger
 Project -	Program 1
+
+References for code -
+1) POSIX Library - http://www.cs.cmu.edu/afs/cs/academic/class/15492-f07/www/pthreads.html
+2) Silberschatz, Galvin, and Gagne's Operating System Concepts
+3) OpenStack discussion forums (multiple answers)
+4) Printf statements are used from sample output provided by Dr. Geiger.
+   http://mjgeiger.github.io/OS/projects/OS_proj1.pdf
+5) Code involves some key developments derived after Discussions with Peter (TA), RakeshKiran Musaley (OS Class collegue).
+6) Sample Executables - Provided by Dr. Geiger
+   http://mjgeiger.github.io/OS/projects/OS_proj1.pdf
+
+Apart from abaove references, no code has been reused. 
 */
 
 #include<sys/types.h>
@@ -16,6 +30,7 @@ Project -	Program 1
 
 int main(int argc, char *argv[] )
 {
+	//following piece of code ensures, number of child processes entered are not greater than 25
 	if (atoi(argv[1]) > 25){
 		printf("Loop count should be less than 25, exiting program \n");
 		return 1;
