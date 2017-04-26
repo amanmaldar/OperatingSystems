01c415117ef2d4da1ab5731b2a621765ab63db13
Aman Maldar <amanmaldar@users.noreply.github.com>
2017-04-26 09:19:53 -0700

    Create OS_program1.c

---
+++ OS_program1.c
@@ -0,0 +1,78 @@
+/*
+Author - 	
+1) Aman Maldar (01670909)
+2) Vishnu Vardhan Reddy Donthi (01646354)
+Course - 	EECE 5730 - Operating Systems
+Instructor - 	Dr. M. Geiger
+Project -	Program 1
+*/
+
+#include<sys/types.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<time.h>
+#include<stdlib.h>
+
+
+int main(int argc, char *argv[] )
+{
+	if (atoi(argv[1]) > 25){
+		printf("Loop count should be less than 25, exiting program \n");
+		return 1;
+	}
+
+	printf("********Project 1 - Question F******** \n");
+	pid_t pid,i,child_pid,master_pid;
+	int status,j,child_number,r;
+	int size = atoi(argv[1]);
+	int mapping_array[size];
+	char name[10];
+
+	master_pid = getpid();
+	for(i = 0; i < atoi(argv[1]); i++) 
+	{
+		if(master_pid == getpid())
+		{		
+			pid = fork();
+			mapping_array[i+1] = pid;
+			//j++;
+		}
+
+		if(pid < 0) {
+			fprintf(stderr, "Fork Failed \n");
+			return 1;
+		} 
+		else if (pid == 0){		//child process 
+
+			printf("Child (%d) : created %d \n", i + 1, getpid());
+			//mapping_array[i+1] = getpid();
+			//printf("Mapping array is (%d)  \n",mapping_array[i+1]);
+			sleep(1);
+			r = (i+1) % 5;
+			if (r == 0) {r =5;}
+			sprintf(name, "./test%d",r);
+			execlp(name,"ls",NULL);
+			exit(0);
+		}
+		
+	}
+	for(i = 0; i < atoi(argv[1]); i++) 
+	{
+
+		child_pid = wait(&status);
+
+		for (j =1 ; j <= atoi(argv[1]); j++)
+		{
+			if (mapping_array[j] == child_pid)
+			{
+				child_number = 	j;				
+				break;					
+			}
+		}
+		printf("Parent know child_id (%d) | child_number (%d) is finished     \n",child_pid,child_number);
+		//printf("Mapping array is (%d)  \n",mapping_array[i+1]);
+	
+	}
+	
+	return 0;
+}
